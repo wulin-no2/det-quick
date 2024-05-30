e17f55e0244b3f3b6b5eda83f04c0a8b0b6c4d5c
Lina <wulinthu@gmail.com>
2024-05-30 02:16:33 +0100

    Add link to homepage

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,15 +1,16 @@
-import Button from "@mui/material/Button";
-import { Box } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 
 function HomePage() {
   return (
     <div className="content">
       <div className="container">
         <Box className="p-4 max-w-lg mx-auto">
-          <h1>THIS IS A WEBSITE</h1>
-          <Button variant="contained" color="primary">
+          <Typography variant="h3" component={Link} href={`/practice`}>
+            Practice Now
+          </Typography>
+          {/* <Button variant="contained" color="primary">
             MUI Button
-          </Button>
+          </Button> */}
         </Box>
       </div>
     </div>
