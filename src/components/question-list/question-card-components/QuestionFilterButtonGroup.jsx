284f201bd93750979b088bce2e3e1ee8fbd5bc8d
Lina <wulinthu@gmail.com>
2024-06-10 18:24:23 +0100

    Modify filter label

--- src/components/question-list/question-card-components/QuestionFilterButtonGroup.jsx
+++ src/components/question-list/question-card-components/QuestionFilterButtonGroup.jsx
@@ -1,24 +1,46 @@
-import PropTypes from 'prop-types';
-import { Button, Box, Typography } from '@mui/material';
+import PropTypes from "prop-types";
+import { Button, Box, Typography } from "@mui/material";
 
-const QuestionFilterButtonGroup = ({ label, buttons, selected, onSelectionChange }) => {
+const getDisplayLabel = (label) => {
+  switch (label) {
+    case "isAsc":
+      return "Sort";
+    case "isCollected":
+      return "Collection";
+    case "isPractice":
+      return "Practice";
+    case "difficultyLevel":
+      return "Difficulty";
+    default:
+      return label;
+  }
+};
+
+const QuestionFilterButtonGroup = ({
+  label,
+  buttons,
+  selected,
+  onSelectionChange,
+}) => {
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'row', // Ensure layout direction is row for side-by-side arrangement
-        alignItems: 'center', // Vertically center align the items
-        mb: 1
+        display: "flex",
+        flexDirection: "row", // Ensure layout direction is row for side-by-side arrangement
+        alignItems: "center", // Vertically center align the items
+        mb: 1,
       }}
     >
       <Typography variant="subtitle1" sx={{ mr: 2 }}>
-        {label}  {/* Display the label */}
+        {getDisplayLabel(label)} {/* Display the label */}
       </Typography>
       {buttons.map((button, index) => (
         <Button
           key={index}
-          variant={String(selected) === String(button) ? 'contained' : 'outlined'}
-          sx={{ mr: 1, height: '32px' }} // Margin right for spacing, height set for uniformity
+          variant={
+            String(selected) === String(button) ? "contained" : "outlined"
+          }
+          sx={{ mr: 1, height: "32px" }} // Margin right for spacing, height set for uniformity
           onClick={() => onSelectionChange(button)} // Use the provided onSelectionChange prop
         >
           {button}
@@ -32,7 +54,7 @@ QuestionFilterButtonGroup.propTypes = {
   label: PropTypes.string.isRequired,
   buttons: PropTypes.arrayOf(PropTypes.string).isRequired,
   selected: PropTypes.string.isRequired,
-  onSelectionChange: PropTypes.func.isRequired
+  onSelectionChange: PropTypes.func.isRequired,
 };
 
 export default QuestionFilterButtonGroup;
