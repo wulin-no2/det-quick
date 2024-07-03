a04ff3abf72286ac1314744ccd59da5442d61658
Swan-Cai <96821426+Swan-Cai@users.noreply.github.com>
2024-07-03 22:00:00 +0100

    Update the WriteAboutThePhotoCard

--- src/components/question-cards/WriteAboutThePhoto.jsx
+++ src/components/question-cards/WriteAboutThePhoto.jsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import { Box, Typography, Divider, TextField, Button } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import AnswerButton from "../common/AnswerButton";
+import ReferenceButton from "../common/ReferenceButton";
 import CardHeader from "../common/question-card-components/CardHeader";
+import { grey } from "@mui/material/colors";
 
 const WriteAboutThePhotoCard = ({
   count,
@@ -15,9 +17,12 @@ const WriteAboutThePhotoCard = ({
   handleNext,
 }) => {
   const [showReferenceAnswer, setShowReferenceAnswer] = useState(false);
+  const [showUserAnswer, setShowUserAnswer] = useState(false);
   const [userAnswer, setUserAnswer] = useState("");
+  const [submittedAnswer, setSubmittedAnswer] = useState("");
   const [error, setError] = useState("");
   const { t } = useTranslation();
+  const textFieldRef = useRef(null);
 
   useEffect(() => {
     console.log("question detail is", questionDetail);
@@ -28,6 +33,10 @@ const WriteAboutThePhotoCard = ({
     setShowReferenceAnswer(!showReferenceAnswer);
   };
 
+  const handleUserAnswerClick = () => {
+    setShowUserAnswer(!showUserAnswer);
+  };
+
   const handleSubmitAnswer = () => {
     if (!userAnswer.trim()) {
       setError(t("Answer cannot be empty, Please write the answer."));
@@ -35,7 +44,8 @@ const WriteAboutThePhotoCard = ({
     }
     console.log("User answer submitted:", userAnswer);
     setError("");
-    handleNext();
+    setSubmittedAnswer(userAnswer);
+    setUserAnswer("");
   };
 
   if (!questionDetail) {
@@ -63,55 +73,52 @@ const WriteAboutThePhotoCard = ({
       <Box
         sx={{
           display: "flex",
-          flexDirection: "column",
+          flexDirection: "row",
+          alignItems: "flex-start",
+          justifyContent: "space-around",
           m: 4,
         }}
       >
-        <Typography
-          variant="h4"
-          gutterBottom
-          sx={{ fontWeight: "bold", opacity: 0.92, mb: 2 }}
-        >
-          {t("Write about the image below.")}
-        </Typography>
-        {questionDetail.questionImageUrl ? (
-          <img
-            src={questionDetail.questionImageUrl}
-            style={{ width: "300px", margin: "auto" }}
-            onError={(e) => {
-              e.target.onerror = null;
-              e.target.src = "/placeholder.svg";
-            }}
-          />
-        ) : (
-          <Typography variant="h6" color="error">
-            {t("Image not available")}
+        <Box sx={{ width: "45%" }}>
+          {questionDetail.questionImageUrl ? (
+            <img
+              src={questionDetail.questionImageUrl}
+              style={{ width: "100%" }}
+              onError={(e) => {
+                e.target.onerror = null;
+                e.target.src = "/placeholder.svg";
+              }}
+            />
+          ) : (
+            <Typography variant="h6" color="error">
+              {t("Image not available")}
+            </Typography>
+          )}
+        </Box>
+        <Box sx={{ width: "45%", textAlign: "left" }}>
+          <Typography
+            variant="h4"
+            gutterBottom
+            sx={{ fontWeight: "bold", opacity: 0.92, mb: 2 }}
+          >
+            {t("Write about the image below.")}
           </Typography>
-        )}
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          pb: 4,
-        }}
-      >
-        <TextField
-          label={t("Write your answer")}
-          multiline
-          rows={4}
-          variant="outlined"
-          value={userAnswer}
-          onChange={(e) => setUserAnswer(e.target.value)}
-          sx={{ width: "80%", mb: 2 }}
-          error={!!error}
-          helperText={error}
-        />
-        <Button variant="contained" onClick={handleSubmitAnswer}>
-          {t("Submit Answer")}
-        </Button>
+          <TextField
+            label={t("Write your answer")}
+            multiline
+            rows={8}
+            variant="outlined"
+            value={userAnswer}
+            onChange={(e) => setUserAnswer(e.target.value)}
+            sx={{ width: "100%", mb: 2 }}
+            inputRef={textFieldRef}
+            error={!!error}
+            helperText={error}
+          />
+          <Button variant="contained" onClick={handleSubmitAnswer}>
+            {t("Submit Answer")}
+          </Button>
+        </Box>
       </Box>
       <Divider sx={{ bgcolor: "grey.100", width: "96%", mx: "auto" }} />
       <Box
@@ -128,21 +135,59 @@ const WriteAboutThePhotoCard = ({
           borderRadius: 1,
         }}
       >
-        <AnswerButton
-          text="Reference Answer"
-          onClick={handleReferenceAnswerClick}
-        />
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <ReferenceButton
+            text="Reference Answer"
+            onClick={handleReferenceAnswerClick}
+          />
+          {submittedAnswer && (
+            <ReferenceButton
+              text="Your Answer"
+              onClick={handleUserAnswerClick}
+            />
+          )}
+        </Box>
         {showReferenceAnswer && (
           <Box
             sx={{
               display: "flex",
-              alignItems: "center",
+              flexDirection: "column",
+              alignItems: "start",
               justifyContent: "center",
               m: 2,
             }}
           >
+            <Typography
+              variant="h7"
+              sx={{ textAlign: "left", p: 2, fontWeight: "bold" }}
+            >
+              {" - "}
+              {t("Reference Answer")}
+            </Typography>
             <Typography variant="subtitle1" sx={{ textAlign: "left", px: 6 }}>
               {questionDetail.referenceAnswer}
+            </Typography>
+          </Box>
+        )}
+        {showUserAnswer && (
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "start",
+              justifyContent: "center",
+              m: 2,
+            }}
+          >
+            <Typography
+              variant="h7"
+              sx={{ textAlign: "left", p: 2, fontWeight: "bold" }}
+            >
+              {" - "}
+              {t("Your Answer")}
+            </Typography>
+            <Typography variant="subtitle1" sx={{ textAlign: "left", px: 6 }}>
+              {submittedAnswer}
             </Typography>
           </Box>
         )}
